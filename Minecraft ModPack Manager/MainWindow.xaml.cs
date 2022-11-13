4feb19a3228cd70e66bd2cc497b502406946c7e0
Camille <camille@edusign.fr>
2022-11-13 18:12:16 +0100

    Finish app

--- Minecraft ModPack Manager/MainWindow.xaml.cs
+++ Minecraft ModPack Manager/MainWindow.xaml.cs
@@ -16,11 +16,23 @@ using System.Windows.Shapes;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using static System.Net.WebRequestMethods;
+using System.Threading;
 
 class MinecraftPathGetter {
     public bool status;
     public string path;
     public string message;
+}
+
+static class InstancesPath {
+    public static string path;
+    public static string modPath;
+}
+
+static class Mods {
+    public static string[] profileMods;
+    public static string[] installedMods;
 }
 
 namespace Minecraft_ModPack_Manager
@@ -30,6 +42,7 @@ namespace Minecraft_ModPack_Manager
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
@@ -44,6 +57,7 @@ namespace Minecraft_ModPack_Manager
 
                 string profilePath = SelectMinecraftProfile(path.path + "\\Instances");
                 Pathes.Text = profilePath;
+                InstancesPath.path = path.path + "\\Instances";
             }
         }
 
@@ -87,23 +101,101 @@ namespace Minecraft_ModPack_Manager
                 return "no instances installed";
             }
 
-            string[] instancesPathCut = new string[instances.Length];
-
-            int index = 0;
             InstanceSelector.Visibility = Visibility.Visible;
-            foreach (string instance in instances) {
-                instancesPathCut[index] = instance.Split("\\").Last();
-                index++;
-            }
-
-            InstanceSelector.ItemsSource = instancesPathCut;
-            return "Ok";
-            // return string.Join(",", instances);
+            InstanceSelector.ItemsSource = SplitAndGetLastvalue(instances);
+            return "Profiles found.";
         }
 
         private void InstanceSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Pathes.Text = (string)InstanceSelector.SelectedValue;
+            Pathes.Text = "Instance selected: " + (string)InstanceSelector.SelectedValue;
+            string workingDirectory = Environment.CurrentDirectory;
+
+            string[] modFolderFiles = Directory.GetFiles(InstancesPath.path + "\\" + (string)InstanceSelector.SelectedValue + "\\mods");
+            string[] modAssets = Directory.GetFiles(Directory.GetParent(workingDirectory)!.Parent!.Parent!.FullName + "\\modList");
+
+            Mods.installedMods = SplitAndGetLastvalue(modAssets);
+            Mods.profileMods = SplitAndGetLastvalue(modFolderFiles);
+
+            bool toUpdate = Enumerable.SequenceEqual(Mods.installedMods, Mods.profileMods);
+
+
+            DifferencesStatus.Visibility= Visibility.Visible;
+            if (toUpdate)
+            {
+                DifferencesStatus.Foreground = Brushes.Green;
+                DifferencesStatus.Text = "Everything is up to date !";
+                ExitButton.Visibility = Visibility.Visible;
+                ExitButton.IsEnabled = true;
+            }
+            else {
+                InstancesPath.modPath = InstancesPath.path + "\\" + (string)InstanceSelector.SelectedValue + "\\mods";
+                DifferencesStatus.Foreground = Brushes.OrangeRed;
+                DifferencesStatus.Text = "New mods are available, would you like to update your profile ?";
+                UpdateButton.Visibility = Visibility.Visible;
+                UpdateButton.IsEnabled = true;
+            }
+        }
+
+        private string[] SplitAndGetLastvalue(string[] stringList) {
+            int index = 0;
+            string[] stringCut = new string[stringList.Length];
+
+            foreach (string str in stringList)
+            {
+                stringCut[index] = str.Split("\\").Last();
+                index++;
+            }
+
+            return stringCut;
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            DirectoryInfo dirProfile = new DirectoryInfo(InstancesPath.modPath);
+            string workingDirectory = Environment.CurrentDirectory;
+            DirectoryInfo dirInstalled = new DirectoryInfo(Directory.GetParent(workingDirectory)!.Parent!.Parent!.FullName + "\\modList");
+
+
+            int maximum = dirProfile.GetFiles().Length + dirInstalled.GetFiles().Length;
+
+            DifferencesStatus.Foreground = Brushes.OrangeRed;
+            DifferencesStatus.Text = "Updating mods ...";
+            Progression.Visibility = Visibility.Visible;
+            Progression.Maximum = maximum;
+            Progression.Value = 0;
+            int counter = 0;
+
+            foreach (FileInfo file in dirProfile.GetFiles())
+            {
+                file.Delete();
+                counter++;
+                Progression.Value = counter;
+                //Thread.Sleep(100);
+            }
+
+            string[] filesToInstall = Directory.GetFiles(Directory.GetParent(workingDirectory)!.Parent!.Parent!.FullName + "\\modList");
+            foreach (string s in filesToInstall)
+            {
+                // Use static Path methods to extract only the file name from the path.
+                string fileName = System.IO.Path.GetFileName(s);
+                string destFile = System.IO.Path.Combine(InstancesPath.modPath, fileName);
+                System.IO.File.Copy(s, destFile, true);
+                counter++;
+                Progression.Value = counter;
+                //Thread.Sleep(100);
+            }
+
+            DifferencesStatus.Foreground = Brushes.Green;
+            DifferencesStatus.Text = "Update finish ! You can close the window by pressing exit button.";
+            ExitButton.Visibility = Visibility.Visible;
+            ExitButton.IsEnabled = true;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            Environment.Exit(1);
         }
     }
 
